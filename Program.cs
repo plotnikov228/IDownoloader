5d982dfa63dd03cb2bb943e42c9538f0823f97bc
plotnikov228 <92577632+plotnikov228@users.noreply.github.com>
2022-03-25 13:20:57 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,60 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Threading;
 using EventAndDelegateHomeWork.InterfaceForStudent;
 
 namespace EventAndDelegateHomeWork
-
 {
-
-
+    /// <summary>
+    /// Создать панель управления вашим обьектом с клавиатуры
+    /// </summary>
+   
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Random rand = new Random();
             InternetFile file = new InternetFile();
-            file.Weight = 500;
-            file.Name = "'Among Us'";
-            int n = 0;
-            int a, a1;
-            a1 = file.Weight;
+            IDisplay display = new IDisplay();
 
-            Console.WriteLine($"Что бы скачать файл {file.Name}, размер - {file.Weight}, введите 1.");
-            string o = Console.ReadLine();
-            if (o == "1")
-            {
-
-                file.Start();
-
-                for (int i = 0; n < a1; i++)
-                {
-                    Thread.Sleep(1000);
-                    a = rand.Next(100);
-                    file.Error(a);
-                    Console.WriteLine("-Скорость-");
-                    Console.WriteLine(a);
-                    n = n + a;
-                    if (n> file.Weight)
-                    {
-
-                        n = file.Weight;
-
-                    }
-                    Console.WriteLine("-Скачано-");
-                    Console.WriteLine(n);
-                    Console.WriteLine(" ");
-                    file.Complete(n);
-                    
-                }
-            }
-
-
-            else Console.WriteLine("-Загрузка отменена-");
-
+            
+            file.MessageEvent += display.Show;
+            file.Start();
+            
+         
         }
-
     }
-
 }
+
